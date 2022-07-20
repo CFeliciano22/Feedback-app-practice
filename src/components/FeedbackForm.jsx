4e1e7baee4748a1c2a2c8e6f4d7f16031e7678c9
Cesar Feliciano <cesarfeliciano1@aol.com>
2022-07-20 08:49:31 -0400

    added side effects with useeffect for the edit cards

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Card from './shared/Card'
-import {useState, useContext} from 'react'
+import {useState, useContext, useEffect} from 'react'
 import Button from './shared/Button'
 import RatingSelect from './RatingSelect' 
 import FeedbackContext from '../context/FeedbackContext'
@@ -13,7 +13,18 @@ function FeedbackForm() {
     const [message, setMessage] = useState('')
     const [rating, setRating] = useState(0)
 
-    const {addFeedback} = useContext(FeedbackContext)
+    const {addFeedback, feedbackEdit} = useContext(FeedbackContext)
+
+    useEffect(() => {
+        if (feedbackEdit.edit === true) {
+            setBtnDisabled(false)
+            setText(feedbackEdit.item.text)
+            setRating(feedbackEdit.item.rating)
+        } else {
+            setText('')
+            setRating(0)
+        }
+    },[feedbackEdit])
 
     const handleTextChange = (e) => {
         if (text=== '') {
--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -2,32 +2,58 @@ import { createContext, useState } from "react";
 import {v4 as uuidv4} from 'uuid'
 
 
+
 const FeedbackContext = createContext();
 
 export const FeedbackProvider = ({ children }) => {
     const [feedback, setFeedback] = useState([
         {
             id: 1,
-            text: "This is a feedback",
+            text: "This is a great product",
             rating: 5,
         },
+        {
+            id: 2,
+            text: "This is a great product",
+            rating: 8,
+        },
+        {
+            id: 3,
+            text: "This is a great product",
+            rating: 10,
+        }
     ]);
 
+    const [feedbackEdit, setFeedbackEdit] = useState({
+        item:{},
+        edit: false,
+    });
+
+    //add feedback
     const addFeedback = (newFeedback) => {
         newFeedback.id = uuidv4()
         setFeedback([newFeedback, ...feedback])
     }
 
+    //delete feedback
     const deleteFeedback = (id) => {
         if (window.confirm('Are you sure you want to delete this feedback?')) {
         setFeedback(feedback.filter(item => item.id !== id))
         }
     }
 
+    //set item to be updated
+    const editFeedback = (item) => {
+        setFeedbackEdit({
+            item: item,
+            edit: true,
+        })
+    }
+
 
     return (
     <FeedbackContext.Provider 
-    value={{feedback, deleteFeedback, addFeedback}}>
+    value={{feedback, deleteFeedback, addFeedback, editFeedback, feedbackEdit}}>
     {children}
     
     
